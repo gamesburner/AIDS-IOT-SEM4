15ddcf0466f92adf5dfb0e5860ccfda1c5ae1013
Anonymous SPPU Student <gamesburner10@gmail.com>
2025-04-29 19:20:45 +0530

    Update pr8.c

--- codes/pr8.c
+++ codes/pr8.c
@@ -3,10 +3,9 @@ void setup() {
 Serial.begin(9600); 
 } 
 void loop() { if 
-"); 
-(Serial.available() > 0) { 
-int num = Serial.parseInt(); 
-Serial.print("I received:
+ (Serial.available() > 0) { 
+ int num = Serial.parseInt(); 
+ Serial.print("I received:"); 
  Serial.println(num); 
 out = num * num; 
 Serial.print("Enter the Number for Square: "); 
