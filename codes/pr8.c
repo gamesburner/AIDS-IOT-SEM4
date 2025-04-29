bbb69b97e2592493cb47921ffbed84ff4718a16e
Anonymous SPPU Student <gamesburner10@gmail.com>
2025-04-29 19:01:46 +0530

    Create pr8.c

---
+++ codes/pr8.c
@@ -0,0 +1,15 @@
+int out; 
+void setup() { 
+Serial.begin(9600); 
+} 
+void loop() { if 
+"); 
+(Serial.available() > 0) { 
+int num = Serial.parseInt(); 
+Serial.print("I received:
+ Serial.println(num); 
+out = num * num; 
+Serial.print("Enter the Number for Square: "); 
+Serial.println(out); 
+} 
+}
