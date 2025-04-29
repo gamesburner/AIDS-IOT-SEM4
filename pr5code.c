87453eb5e3bcea13ff55f84bc1687f61b0be1f96
Anonymous SPPU Student <gamesburner10@gmail.com>
2025-04-29 09:48:09 +0530

    Create pr5code.c

---
+++ pr5code.c
@@ -0,0 +1,27 @@
+void setup()
+
+{
+
+Serial.begin(9600),
+
+pinMode(2,OUTPUT);
+
+}
+
+Void loop()
+
+{
+
+erial.println("HIGH");
+
+digitalWrite(2.1),
+
+delay(1000);
+
+Serial print("LOW");
+
+digitalWrite(2,0);
+
+delay(1000);
+
+}
