f53e4a9a931dc925da4842b2405240c11a809a6e
Anonymous SPPU Student <gamesburner10@gmail.com>
2025-04-30 10:10:32 +0530

    Create pr12code.c

---
+++ codes/pr12code.c
@@ -0,0 +1,19 @@
+Void setup()
+ Serial.begin(9600);
+ (12, OUTPUT);
+ pinMode=(13, OUTPUT);
+ }
+ void loop()
+ int s1-analogRead(40);
+ Serial.println(s1);
+ delay(200);
+ if(s1>100)
+ {
+ digitalWrite(12,LOW);
+ digitalWrite(13, LOW);
+ }
+ else
+ digitalwrite(12, HIGH);
+ digitale ite(13, HIGH);
+ }
+}
