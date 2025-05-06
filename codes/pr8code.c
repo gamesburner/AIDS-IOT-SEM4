94e050755658543fd78e774513eddac272742552
Anonymous SPPU Student <gamesburner10@gmail.com>
2025-05-06 21:22:42 +0530

    Update pr8code.c

--- codes/pr8code.c
+++ codes/pr8code.c
@@ -1,14 +1,17 @@
-int out; 
-void setup() { 
-Serial.begin(9600); 
+int out;  
+void setup()  
+{  
+Serial.begin(9600);  
+}  
+void loop()  
+{  
+if(Serial.available() > 0)  
+{  
+int num = Serial.parseInt();  
+Serial.print("I received:");  
+Serial.println(num);  
+out = num * num;  
+Serial.print("Enter the Number for Square: ");  
+Serial.println(out);  
+}  
 } 
-void loop() { if 
- (Serial.available() > 0) { 
- int num = Serial.parseInt(); 
- Serial.print("I received:"); 
- Serial.println(num); 
-out = num * num; 
-Serial.print("Enter the Number for Square: "); 
-Serial.println(out); 
-} 
-}
